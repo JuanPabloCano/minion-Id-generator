7d29f33dae046c109ee91f7f4160b4912f67464a
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-27 19:58:00 -0500

    Feat: added welcome message and Http responses

--- src/application/shared/utils.js
+++ src/application/shared/utils.js
@@ -1,10 +1,17 @@
 export const SERVER_UTILS = {
   PORT: 8081,
+  WELCOME_MESSAGE: 'Welcome to the Minion Id Changer App, to find all minions please visit https://localhost:8081/minion or add minion/{number}, from 0 to 10000, to generate a Minion with a new Id',
 };
 
 export const DI_UTILS = {
   MINION_CONTROLLER: 'minionController',
-  PG_DATABASE: 'PgDatabase',
-  MINION_DAO: 'minionDao',
-  MINION_PG_ADAPTER: 'minionPgAdapter',
+};
+
+export const HTTP_STATUS_CODE = {
+  OK: 200,
+  BAD_REQUEST: 400,
+};
+export const HTTP_ERR0R_MESSAGE = {
+  VALIDATION_ERROR: 'Validation error',
+  ID_ALREADY_CREATED: 'ID already created',
 };
