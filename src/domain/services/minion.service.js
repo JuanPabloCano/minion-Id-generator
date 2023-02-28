815aae1e21ec4dfa0d5a51ba06ed63e48ad6fbdd
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-27 19:59:36 -0500

    Feat: added private methods to transform commander lambda string and findAll minions method

--- src/domain/services/minion.service.js
+++ src/domain/services/minion.service.js
@@ -3,17 +3,48 @@ import { Minion } from '../models/Minion.js';
 export class MinionService {
 
   #minionPgAdapter;
+  #readCommanderLambdaStringService;
+  #POSITIONS;
+  #MAX_ALLOWED_LENGTH;
 
-  constructor({ minionPgAdapter }) {
+  constructor({ minionPgAdapter, readCommanderLambdaStringService }) {
     this.#minionPgAdapter = minionPgAdapter;
+    this.#readCommanderLambdaStringService = readCommanderLambdaStringService;
+    this.#POSITIONS = 5;
+    this.#MAX_ALLOWED_LENGTH = 10000;
   }
 
-  getUsers({ id, paramNumber }) {
-    this.#minionPgAdapter.test();
-    const minion = new Minion()
-        .setMinionAsk(id)
-        .setRelatedCode(paramNumber)
-        .build();
-    return [ 'Alice', 'Bob', 'Charlie', minion ];
+  async updateMinionId(codeIdAsked) {
+    try {
+      const primeCode = await this.#transformCommanderLambdaString(codeIdAsked);
+      return this.#minionPgAdapter.create(
+          new Minion()
+              .setMinionAsk(codeIdAsked)
+              .setRelatedCode(primeCode)
+              .build(),
+      );
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.#minionPgAdapter.findAll();
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  async #transformCommanderLambdaString(codeIdAsked) {
+    const commanderLambdaString = await this.#readCommanderLambdaStringService.execute();
+    if (codeIdAsked > this.#MAX_ALLOWED_LENGTH || codeIdAsked < 0) {
+      throw new Error('Invalid Minion Id Asked');
+    }
+    return this.#shortenCommanderLambdaString(commanderLambdaString, Number(codeIdAsked));
+  }
+
+  #shortenCommanderLambdaString(token, index) {
+    return token.slice(index, index + this.#POSITIONS);
   }
 }
