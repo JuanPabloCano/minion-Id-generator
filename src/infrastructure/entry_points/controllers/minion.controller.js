d9b394ac49e631be1001493649d5c10b98dd0111
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-27 20:06:39 -0500

    Feat: Added findAll method and refactor updateMinionId method

--- src/infrastructure/entry_points/controllers/minion.controller.js
+++ src/infrastructure/entry_points/controllers/minion.controller.js
@@ -1,3 +1,5 @@
+import { HTTP_ERR0R_MESSAGE, HTTP_STATUS_CODE } from '../../../application/shared/utils.js';
+
 export class MinionController {
   #minionService;
 
@@ -5,8 +7,31 @@ export class MinionController {
     this.#minionService = minionService;
   }
 
-  getUsers(req, res) {
-    const users = this.#minionService.getUsers(req.query);
-    return res.json(users);
+  async updateMinionId(req, res) {
+    try {
+      const { codeIdAsked } = req.params;
+      const minion = await this.#minionService.updateMinionId(codeIdAsked);
+      return res
+          .status(HTTP_STATUS_CODE.OK)
+          .json(minion);
+    } catch (err) {
+      if (err.message === HTTP_ERR0R_MESSAGE.VALIDATION_ERROR) {
+        return res
+            .status(HTTP_STATUS_CODE.BAD_REQUEST)
+            .json({ error: HTTP_ERR0R_MESSAGE.ID_ALREADY_CREATED });
+      }
+      return res.json({ error: err.message });
+    }
+  }
+
+  async findAll(_req, res) {
+    try {
+      const minions = await this.#minionService.findAll();
+      return res
+          .status(HTTP_STATUS_CODE.OK)
+          .json(minions);
+    } catch (err) {
+      return res.json({ error: err });
+    }
   }
 }
